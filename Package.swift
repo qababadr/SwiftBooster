5f5fb59b0faee579fda82a122687602b471ffc48
badr qaba <qababadr@gmail.com>
2026-07-08 23:04:11 +0100

    fixing macros

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
     name: "SwiftBooster",
-    platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftBooster",
             targets: ["SwiftBooster"]
-        ),
-        .executable(
-            name: "SwiftBoosterClient",
-            targets: ["SwiftBoosterClient"]
-        ),
+        )
     ],
     dependencies: [
         .package(
@@ -29,9 +29,7 @@ let package = Package(
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
+
         .macro(
             name: "SwiftBoosterMacros",
             dependencies: [
@@ -42,17 +40,18 @@ let package = Package(
             ]
         ),
 
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(
             name: "SwiftBooster",
             dependencies: [
                 "SwiftBoosterMacros",
-                "Swinject"
+                .product(name: "Swinject", package: "Swinject")
             ]
         ),
 
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "SwiftBoosterClient", dependencies: ["SwiftBooster"]),
-
+        // Optional. Keep only for local development.
+        .executableTarget(
+            name: "SwiftBoosterClient",
+            dependencies: ["SwiftBooster"]
+        )
     ]
 )
